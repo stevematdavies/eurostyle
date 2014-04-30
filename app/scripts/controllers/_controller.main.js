ca8bda93de1753c5ff725b9638ff4c4d49a7a95c
stevematdavies <stevematdavies@gmail.com>
2014-04-30 21:11:27 +0300

    setting up singletons

--- app/scripts/controllers/_controller.main.js
+++ app/scripts/controllers/_controller.main.js
@@ -1,24 +1,5 @@
 "use strict";
 
-app.controller('MainCtrl',function($scope,UserService){
-    $scope.app = {};
-    $scope.app.users = [];
-    $scope.app.currentuser ={};
-    $scope.app.name ="EuroStyle";
-
-    console.log($scope.app.users);
-
-    $scope.app.newuser = function(user){
-        $scope.app.currentuser = UserService.createUser(user);
-        console.log($scope.app.currentuser);
-        
-
-        
-    }
-
-    
-
-
-
+app.controller('MainCtrl',function($scope,UserService,DataService){
 
 });
--- app/scripts/services/_service.firebase.js
+++ app/scripts/services/_service.firebase.js
@@ -2,6 +2,10 @@
 
 app.factory('DataService',['$firebase','FIREBASE_URI',function($firebase,FIREBASE_URI){
         var ref = new Firebase(FIREBASE_URI);
-        var data = $firebase(ref);
+        return {
+
+        }
+
+
 
 }]);
